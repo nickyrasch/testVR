c5cc50dfd6522b99f38b56253b6bf6007e08e96c
Nicky Rasch <nickynrasch@gmail.com>
2018-11-22 16:04:36 -0800

    Initial commit

---
+++ Source/testVR/testVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "testVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTVR_API AtestVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
